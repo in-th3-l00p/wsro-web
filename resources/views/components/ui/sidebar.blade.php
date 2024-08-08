b06701c6ab12194219c19344f1d33d88d8427598
in-th3-l00p <indianucode0303@gmail.com>
2024-08-08 18:33:55 +0300

    updated sidebar

--- resources/views/components/ui/sidebar.blade.php
+++ resources/views/components/ui/sidebar.blade.php
@@ -1,6 +1,27 @@
 <aside
-    x-data="{ open: true }"
+    x-data="{ open: false }"
     class="bg-rose-700 min-h-screen flex gap-2 shadow-rose-950 shadow-lg"
 >
-    {{ $slot }}
+    <nav
+        x-show="open"
+        class="flex flex-col"
+        style="display: none;"
+    >
+        {{ $slot }}
+    </nav>
+
+    <button
+        @click="open = !open"
+        type="button"
+        x-bind:title="open ? 'Close sidebar' : 'Open sidebar'"
+        @class([
+            "rounded-md p-4 m-2 w-12 h-12",
+            "flex justify-center items-center",
+            "hover:bg-rose-500 hover:scale-105 transition-all"
+        ])
+        x-bind:class="open ? 'absolute' : ''"
+    >
+        <i x-show="open" class="sidebar-icon fa-regular fa-3xl fa-square-caret-left"></i>
+        <i x-show="!open" class="sidebar-icon fa-regular fa-3xl fa-square-caret-right"></i>
+    </button>
 </aside>
