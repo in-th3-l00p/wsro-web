87896313d1a3f167e339037b0bc7ebc7667f57f1
in-th3-l00p <indianucode0303@gmail.com>
2024-08-10 23:31:06 +0300

    updated test projects display

--- resources/views/components/admin/test-projects/test-project-display.blade.php
+++ resources/views/components/admin/test-projects/test-project-display.blade.php
@@ -1,12 +1,12 @@
 <div
     type="button"
     @class([
-        "bg-white p-8 rounded-md shadow-md h-96 flex flex-col",
+        "bg-white p-8 rounded-md shadow-md h-96 flex flex-col gap-4",
         "hover:scale-105 hover:shadow-lg transition-all"
     ])
 >
     @if ($testProject->tags()->count() > 0)
-        <div class="flex flex-wrap gap-2 mb-4">
+        <div class="flex flex-wrap gap-2">
             @foreach ($testProject->tags()->get() as $tag)
                 <div class="rounded-2xl bg-rose-600 text-white px-3 py-1">
                     {{ $tag->name }}
@@ -15,8 +15,10 @@
         </div>
     @endif
 
-    <h2 class="text-xl font-bold mb-4">{{ $testProject->title }}</h2>
-    <div class="mb-auto">{!! $testProject->description !!}</div>
+    <h2 class="text-xl font-bold">{{ $testProject->title }}</h2>
+    <div class="mb-auto h-18 overflow-hidden line-clamp-3 text-ellipsis">
+        {!! $testProject->description !!}
+    </div>
 
     <div class="flex gap-4">
         <a
--- resources/views/components/admin/test-projects/trashed-test-project-display.blade.php
+++ resources/views/components/admin/test-projects/trashed-test-project-display.blade.php
@@ -1,12 +1,24 @@
 <div
     type="button"
     @class([
-        "bg-white p-8 rounded-md shadow-md h-96 flex flex-col",
+        "bg-white p-8 rounded-md shadow-md h-96 flex flex-col gap-4",
         "hover:scale-105 hover:shadow-lg transition-all"
     ])
 >
+    @if ($testProject->tags()->count() > 0)
+        <div class="flex flex-wrap gap-2">
+            @foreach ($testProject->tags()->get() as $tag)
+                <div class="rounded-2xl bg-rose-600 text-white px-3 py-1">
+                    {{ $tag->name }}
+                </div>
+            @endforeach
+        </div>
+    @endif
+
     <h2 class="text-xl font-bold mb-4">{{ $testProject->title }}</h2>
-    <div class="mb-auto">{!! $testProject->description !!}</div>
+    <div class="mb-auto h-18 overflow-hidden line-clamp-3 text-ellipsis">
+        {!! $testProject->description !!}
+    </div>
 
     <div class="flex gap-4">
         <form
