de9db91f7e5beb6805a4324d6fd0250cc9c0c395
in-th3-l00p <indianucode0303@gmail.com>
2024-08-11 12:08:02 +0300

    creation of tags

--- app/Http/Controllers/Admin/TestProjectTagController.php
+++ app/Http/Controllers/Admin/TestProjectTagController.php
@@ -13,9 +13,29 @@ class TestProjectTagController extends Controller
     }
 
     public function create(TestProject $testProject) {
+        return view("admin.test-projects.tags.create", [
+            "testProject" => $testProject
+        ]);
     }
 
     public function store(TestProject $testProject, Request $request) {
+        $request->validate([
+            "name" => "required|max:255"
+        ]);
+        $tag = TestProjectTag::query()
+            ->where("name", "=", $request->name)
+            ->first();
+        if (!$tag)
+            $tag = TestProjectTag::create([
+                "name" => $request->name
+            ]);
+        $testProject->tags()->attach($tag);
+        return redirect()
+            ->route("admin.test-projects.show", [
+                "test_project" => $testProject
+            ])
+            ->with([ "success" => __("Tag added!") ]);
+
     }
 
     public function show(
---
+++ resources/views/admin/test-projects/tags/create.blade.php
@@ -0,0 +1,53 @@
+@extends("layouts.main")
+
+@section("content")
+    <x-admin.container
+        :title="__('Add tag to') . ' \'\'' . $testProject->title . '\'\''"
+        :breadcrumbPath="[
+            [ 'href' => route('admin.dashboard'), 'name' => __('Dashboard') ],
+            [ 'href' => route('admin.test-projects.index'), 'name' => __('Test projects') ],
+            [
+                'href' => route('admin.test-projects.show', [ 'test_project' => $testProject ]),
+                'name' => __('Test project') . ' \'\'' . $testProject->title . '\'\''
+            ],
+            [ 'name' => __('Add tag') ],
+        ]"
+    >
+        <x-slot:subtitle>
+            <x-admin.container-subtitle>
+                {{ __("Add or create a new tag") }}
+            </x-admin.container-subtitle>
+        </x-slot:subtitle>
+
+        <form
+            action="{{ route("admin.test-projects.tags.store", [
+                "test_project" => $testProject
+            ]) }}"
+            method="post"
+            class="max-w-xl"
+        >
+            @csrf
+
+            <x-admin.errors-alert :errors="$errors" />
+
+            <div class="form-group mb-8">
+                <label for="name" class="label w-16">{{ __("Name") }}:</label>
+                <input
+                    type="text" name="name" id="name"
+                    @class(["input", "ring-2 ring-rose-600" => $errors->has("name")])
+                    placeholder="{{ __("Tag's name") }}"
+                    value="{{ old("name") }}"
+                    autocomplete="off"
+                >
+            </div>
+
+            <button
+                type="submit"
+                class="btn"
+                title="{{ __("Add") }}"
+            >
+                {{ __("Add") }}
+            </button>
+        </form>
+    </x-admin.container>
+@endsection
