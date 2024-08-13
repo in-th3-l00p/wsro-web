fe1297391599a70e290daca5f2f60f47b31cb05d
Tisca Catalin <admin@tiscacatalin.com>
2024-08-13 21:45:58 +0000

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/fontawesome/app.fontawesome', 'resources/js/app.js'],
+            input: [
+		    'resources/js/app.js',
+		    'resources/scss/app.scss',
+		    'resources/scss/components/attachment.scss',
+		    'resources/js/admin/test-projects/attachments/create.js'
+	    ],
             refresh: true,
         }),
     ],
