80c43d7f6826c67f8dd7920c49b010dc1eabec1b
in-th3-l00p <indianucode0303@gmail.com>
2024-08-14 23:05:21 +0300

    updated dashboard breadcrumbs

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,7 +1,12 @@
 @extends("layouts.main")
 
 @section("content")
-    <x-user.layout :title="__('Dashboard')">
+    <x-user.layout
+        :title="__('Hello') . ', ' . request()->user()->name . '!'"
+        :breadcrumbPath="[
+            [ 'name' => __('Dashboard') ],
+        ]"
+    >
         <p>fdsafdsagfdsafs</p>
     </x-user.layout>
 @endsection
